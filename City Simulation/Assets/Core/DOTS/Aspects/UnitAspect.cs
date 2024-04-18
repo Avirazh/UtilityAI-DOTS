a836220503fd9f8297b6ef7fdd16f22d9f94f511
Avirazh <murmin03@gmail.com>
2024-04-19 00:52:22 +0300

    project initialized

---
+++ City Simulation/Assets/Core/DOTS/Aspects/UnitAspect.cs
@@ -0,0 +1,16 @@
+using ProjectDawn.Navigation;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public readonly partial struct UnitAspect : IAspect
+{
+    public readonly Entity Entity;
+
+    private readonly RefRO<MovableTag> _movableTag;
+    private readonly RefRW<AgentBody> _agentBody;
+
+    public void SetDestination(float3 destination)
+    {
+        _agentBody.ValueRW.SetDestination(destination);
+    }
+}
---
+++ City Simulation/Assets/Core/DOTS/Authoring/UnitAuthoring.cs
@@ -0,0 +1,17 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class UnitAuthoring : MonoBehaviour
+{
+    public GameObject UnitPrefab;
+    public class UnitBaker : Baker<UnitAuthoring>
+    {
+        public override void Bake(UnitAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+
+            AddComponentObject(entity, new UnitPrefabComponent { Value = authoring.UnitPrefab});
+            AddComponent(entity, new MovableTag());
+        }
+    }
+}
